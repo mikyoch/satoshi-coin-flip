61ac003eec37164dc672d89901108a45a4073e50
Alina-chan <alinabalaur@hotmail.com>
2022-11-17 10:04:44 +0200

    [TLSC-19] Add TailwindCSS + config

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,10 +1,16 @@
-
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <div>
-       Time locked satoshi coin flip
+        <h1 class="text-3xl font-bold underline bg-black text-white">
+          This is a heading
+        </h1>
+        <p>
+        Time locked satoshi coin flip
+        </p>
+       
       </div>
     </>
   )
