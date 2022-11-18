f926148d5d5472a3e4d5b8a183925cb793c14bb7
Stefanos Pleros <stefanos@mystenlabs.com>
2022-11-18 13:31:36 +0200

    Created 3 new route files to house the start, end and game routes. Created a central services file so that the same SatoshiGameService storage is used across all files

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
-import express, { Express, Request, Response } from "express";
-import { notFound, errorHandler, checkStart, checkEnd } from "./middleware";
+import express, { Express } from "express";
+import { notFound, errorHandler } from "./middleware";
 import * as dotenv from "dotenv";
 dotenv.config();
-import SatoshiGameService from "./services/SatoshiGameService";
+
+import startRoutes from "./routes/Start";
+import endRoutes from "./routes/End";
+import gameRoutes from "./routes/Game";
 
 const app: Express = express();
-const GameService = new SatoshiGameService();
 const port = process.env.PORT;
 
 app.use(
@@ -17,54 +19,11 @@ app.use(
 // we can use json requests if we prefer
 // app.use(express.json({}));
 
-app.post("/start", checkStart, async (req: Request, res: Response) => {
-  console.log("body:", req.body);
+app.use(startRoutes);
 
-  try {
-    let gameId = await GameService.createGame(req.body.minBet, req.body.maxBet);
-    res.status(200);
-    res.json({
-      gameId,
-    });
-  } catch (e) {
-    console.error("Bad things have happened:", e);
-    res.status(500);
-    res.json({
-      error: e,
-    });
-  }
-});
+app.use(endRoutes);
 
-app.post("/end", checkEnd, async (req: Request, res: Response) => {
-  console.log("body:", req.body);
-
-  try {
-    let response = await GameService.endGame(req.body.gameId);
-    res.status(200);
-    res.json({
-      playerWon: response,
-    });
-  } catch (e) {
-    console.error("Bad things have happened:", e);
-    res.status(500);
-    res.json({
-      error: e,
-    });
-  }
-});
-
-app.get("/games", (req: Request, res: Response) => {
-  res.status(200);
-
-  const gameIds = GameService.getGameIds();
-  res.json({
-    gameIds
-  })
-});
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Satoshi API saying hello :)");
-});
+app.use(gameRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
