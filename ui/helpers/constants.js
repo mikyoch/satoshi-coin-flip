a957ef05b3a7d927ceceb2ae16cc52bbb58464a4
Alina-chan <alinabalaur@hotmail.com>
2022-11-24 15:39:29 +0200

    Head/Tails functionality and TX calls

--- ui/helpers/constants.js
+++ ui/helpers/constants.js
@@ -1,1 +1,2 @@
 export const COIN = {HEADS: 0, TAILS: 1};
+export const PACKAGE = process.env.NEXT_PUBLIC_PACKAGE;
--- ui/pages/_app.jsx
+++ ui/pages/_app.jsx
@@ -1,12 +1,13 @@
-import React, { Component, useMemo } from "react";
+import React, { Component, useMemo, useState } from "react";
 import "../styles/globals.css";
 import { WalletProvider } from "@mysten/wallet-adapter-react";
 import { WalletStandardAdapterProvider } from "@mysten/wallet-adapter-all-wallets";
-import PlayButton from "../components/PlayButton";
+import PlayGameButton from "../components/PlayGameButton";
 import { COIN } from "../helpers/constants";
 
 // Components
 import { Header } from "../components/Header";
+import { NewGameButton } from "../components/NewGameButton";
 
 function MyApp({ Component, pageProps }) {
   const adapters = useMemo(
@@ -16,13 +17,22 @@ function MyApp({ Component, pageProps }) {
     ],
     []
   );
+  const [newGame, setNewGame] = useState(null);
 
   return (
     <WalletProvider adapters={adapters}>
       <Header />
       <div className="App h-screen flex flex-col items-center justify-center bg-faint-blue">
-        <PlayButton coinSide={COIN.HEADS} />
-        <PlayButton coinSide={COIN.TAILS} />
+        {!newGame ? (
+          // Display the New Game button initially
+          <NewGameButton setGameId={setNewGame} />
+        ) : (
+          // Once the gameID is set, show the user the Heads/Tails buttons
+          <>
+            <PlayGameButton coinSide={COIN.HEADS} />
+            <PlayGameButton coinSide={COIN.TAILS} />
+          </>
+        )}
       </div>
     </WalletProvider>
   );
