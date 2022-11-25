8b65e88c92c4cc8133d543bab317fbb009186f45
Stefanos Pleros <stefanos@mystenlabs.com>
2022-11-25 12:27:40 +0200

    getPlayCoin function now gathers all small coins that are lower than the playValue threshold and uses them in the paySui call. getLargestCoin method is now private since its not used out of class anymore

--- api/services/SuiService.ts
+++ api/services/SuiService.ts
@@ -9,7 +9,6 @@ import {
 } from "@mysten/sui.js";
 
 interface SuiServiceInterface {
-  getLargestBankCoin(): Promise<any>;
   getPlayCoin(): Promise<string>;
   executeMoveCall(
     packageObjId: string,
@@ -86,7 +85,7 @@ class SuiService implements SuiServiceInterface {
     });
   }
 
-  public async getLargestBankCoin(): Promise<{ id: string; balance: number }> {
+  private async getLargestBankCoin(): Promise<{ id: string; balance: number }> {
     // this function is no longer called from the GameService so there is no need
     // to perform a check here. Check has been transfered to getPlayCoin function
     // const didFund = await this.fundBankAddressIfGasLow();
@@ -111,7 +110,7 @@ class SuiService implements SuiServiceInterface {
   }
 
   private async splitCoins(
-    coinObjectId: string,
+    coinObjectIds: string[],
     recipient: string,
     splitObjects: number,
     splitValue: number
@@ -120,7 +119,7 @@ class SuiService implements SuiServiceInterface {
     const splitAmounts: number[] = Array(splitObjects).fill(splitValue);
 
     return this.signer.paySui({
-      inputCoins: [coinObjectId],
+      inputCoins: coinObjectIds,
       recipients,
       amounts: splitAmounts,
       gasBudget: 100000,
@@ -141,17 +140,20 @@ class SuiService implements SuiServiceInterface {
         // Finding the largest coin to use as gas
         const gasCoin = await this.getLargestBankCoin();
 
-        // Checking how many coins of playValue balance can be created
+        // Find all coins lower than playValue. Used for recycling
+        const smallCoins = coins.filter((coin) => coin.balance < 5000);
+        const smallCoinIds = smallCoins.map((coin) => coin.id);
+
+        // Checking how many coins of playValue balance can be created from the gas coin
         let maxSplit = Math.floor(gasCoin.balance / playValue);
         // If we can create equal or more than 20 then we attempt to create 20
         // @todo: this is a bit abstract
         // @todo: maybe fund call should be used here instead of using it at the begining of the function
         let finalSplit = 0;
         if (maxSplit >= 20) finalSplit = 20;
-        // @todo: maybe we should pass here coins that have balance lower than a threshold. That threshold could be playValue
-        // this resolves the issue of only having unsuable coins that have a balance that is lower than the threshold
+
         this.splitCoins(
-          gasCoin.id,
+          [gasCoin.id, ...smallCoinIds],
           String(process.env.BANKER_ADDRESS),
           finalSplit,
           playValue
