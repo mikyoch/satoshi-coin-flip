d1078b0494fa3d34061dc5904bbf77f77e298cdd
alex <alex@mystenlabs.com>
2022-11-30 15:47:38 +0200

    Change Explorer link to show custome text, add player choice -- initial

--- ui/components/ExplorerLink.jsx
+++ ui/components/ExplorerLink.jsx
@@ -1,21 +1,23 @@
 // import { useState } from "react";
 
-export default function ExplorerLink({ id, type }) {
+export default function ExplorerLink({ id, text, type }) {
   // const [showTooltip, setShowTooltip] = useState(false);
 
   let url = "https://explorer.sui.io/";
-  if (type === "object") url += "objects/";
-  else if (type === "transaction") url += "transactions/";
-  else if (type === "address") url += "addresses/";
+  // if (type === "object") url += "objects/";
+  // else if (type === "transaction") url += "transactions/";
+  // else if (type === "address") url += "addresses/";
+
   // adding an else just to be able to show `won#gameId`
-  else url += "objects/";
+  // else url += "objects/";
+  url += `${type}/`;
   url += `${encodeURIComponent(id)}`;
   // what will user see
   const isAddress = type === "address";
   const playerWin = type === "win";
   const playerLoss = type === "loss";
   const isObject = type === "object";
-  const shownId = id;
+
 
   const renderColor = () => {
     if (!isAddress && !isObject) {
@@ -31,27 +33,27 @@ export default function ExplorerLink({ id, type }) {
   return (
     <>
       <div className="first-of-type:text-sui-sky text-sui-text-light">
-        <a href={url} target="_blank">
+        <a href={url} target="_blank" referrer="noreferrer">
           {!isAddress ? (
             <>
               {!playerWin && !playerLoss && isObject ? (
                 <>
                   <span className={`${renderColor()} + pr-1 text-sm`}>
-                    {`${type.charAt(0).toUpperCase() + type.slice(1)}:`}
+                    {`${text}:`}
                   </span>
-                  <span className="text-inherit/50 text-sm">{`${shownId}`}</span>
+                  <span className="text-inherit/50 text-sm">{`${id}`}</span>
                 </>
               ) : (
                 <>
                   <span className={`${renderColor()} + pr-1`}>
-                    {`${type.charAt(0).toUpperCase() + type.slice(1)}:`}
+                    {`${text}:`}
                   </span>
-                  <span className="text-inherit/50">{`${shownId}`}</span>
+                  <span className="text-inherit/50">{`${id}`}</span>
                 </>
               )}
             </>
           ) : (
-            <span className="text-inherit/50">{`${shownId}`}</span>
+            <span className="text-inherit/50">{`${text}`}</span>
           )}
         </a>
       </div>
