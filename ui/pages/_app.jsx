07eeaf59c5e33a2b5291940e7d9870a1ede74048
Alina-chan <alinabalaur@hotmail.com>
2022-12-01 18:26:38 +0200

    Add game description box

--- ui/pages/_app.jsx
+++ ui/pages/_app.jsx
@@ -18,6 +18,7 @@ import Footer from "../components/Footer";
 import GameStatus from "../components/GameStatus";
 import TailsSvg from "../public/svg/capy.svg";
 import HeadsSvg from "../public/svg/capy-text.svg";
+import GameDetails from "../components/GameDetails";
 
 function MyApp() {
   // wallet provider
@@ -38,6 +39,7 @@ function MyApp() {
   const [playerGameRes, setPlayerGameRes] = useState(null);
   const [coinSide, setCoinSide] = useState(null);
   const [showChoice, setShowChoice] = useState(null);
+  const [showGameDetails, setShowGameDetails] = useState(false);
 
   // always make sure to end running games
   useEffect(() => {
@@ -69,7 +71,6 @@ function MyApp() {
     notifyPlayResult(`You played ${coinSide}`, playerWon);
     setPlayerGameRes(playerWon);
     setCoinSide(choice);
-    // setShowChoice(null);
     setIsLoading(false);
     if (playerWon)
       setHistory((old) => [
@@ -96,14 +97,82 @@ function MyApp() {
     finish(choice);
   };
 
+  let gameDetailsHeight = "";
+  const handleGameDetailsOpen = () => {
+    setShowGameDetails(true);
+    gameDetailsHeight = "h-full";
+  };
+
+  const handleGameDetailsClose = () => {
+    setShowGameDetails(false);
+    gameDetailsHeight = "h-4";
+  };
+
   return (
     <>
       <Toaster />
       <WalletProvider adapters={adapters}>
-        <div className="App h-screen bg-faint-blue flex flex-col justify-between items-stretch">
+        <div className="App bg-gradient-to-b from-faint-blue to-faint-blue/50 flex flex-col justify-between items-stretch">
           <Header />
           <div className="w-full mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <div className="px-4 py-6 sm:px-0">
+              <div className="flex">
+                <div className="flex flex-col w-full items-center justify-center pb-6 px-4">
+                  <h2 className="text-xl font-semibold">Read game details</h2>
+                  {showGameDetails ? (
+                    <>
+                      <GameDetails boxHeight={gameDetailsHeight} />
+                      <span className="mt-2 w-[35px] h-[2px] bg-amber rounded-full"></span>
+                      <button
+                        className="cursor-pointer pt-2 flex flex-col justify-center items-center"
+                        onClick={handleGameDetailsClose}
+                      >
+                        {" "}
+                        <span className="text-xs">Collapse</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="mt-2 w-4 h-4 animate-bounce"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
+                          />
+                        </svg>
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <span className="mt-2 w-[35px] h-[2px] bg-amber rounded-full"></span>
+                      <button
+                        className="cursor-pointer pt-2 flex flex-col justify-center items-center"
+                        onClick={handleGameDetailsOpen}
+                      >
+                        {" "}
+                        <span className="text-xs">Expand</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="mt-2 w-4 h-4 animate-bounce"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+                          />
+                        </svg>
+                      </button>
+                    </>
+                  )}
+                </div>
+              </div>
               <div className="flex flex-col items-center justify-center text-center pb-5">
                 <span>
                   Current Game:
