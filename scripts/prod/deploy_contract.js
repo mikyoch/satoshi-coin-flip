62b4b81098903b7da9fb8ca3e7e4af8a40fed9c7
Stefanos Pleros <stefanos@mystenlabs.com>
2022-12-05 18:31:19 +0200

    created getPrivateKey method for any keypair so that we can save the private key of the account we generate

--- scripts/prod/deploy_contract.js
+++ scripts/prod/deploy_contract.js
@@ -10,35 +10,28 @@ const {
 } = require("@mysten/sui.js");
 const bytecode = require("../../satoshi_flip/base64Bytecode.json");
 
-// @todo: should be removed, temporarily here
-function base64KeyStoreToPrivateKeyBase64(base64KeyStore) {
-  let buf_array = fromB64(base64KeyStore);
-  let arr = Array.from(buf_array);
-  arr.shift();
-  let pub_key = arr.slice(0, 32);
-  let privkey = arr.slice(32, 64);
-  console.log("private key array", privkey);
-  const keypair = Ed25519Keypair.fromSeed(Uint8Array.from(privkey));
-  console.log("keypair", keypair);
-  return keypair;
+function getPrivateKey(keypair) {
+  const keyPair = JSON.stringify(keypair);
+  const secretKey = JSON.parse(keyPair)?.keypair?.secretKey;
+  let arr = Array.from(Object.values(secretKey));
+  let privkey = arr.slice(0, 32);
+  return privkey;
 }
 
 async function createFundedHouseAccount() {
   const provider = new JsonRpcProvider(Network.DEVNET);
-  const keypair = base64KeyStoreToPrivateKeyBase64(
-    "AFO1xQq1nCy8CUG3ulY0v7uiMlU+76lKk2fPgx2Y0W0vEEw7lxszJkqHO1dLV3F3nL3EjwL5jF13JYuY7nrqcug="
-  );
-  //   const keypair = new Ed25519Keypair();
+  const keypair = new Ed25519Keypair();
+
+  const privKey = getPrivateKey(keypair);
+
   const signer = new RawSigner(keypair, provider);
   const address = await signer.getAddress();
-  console.log("address", address);
   //   await provider.requestSuiFromFaucet(address);
-  return signer;
+  return { signer, address, privKey };
 }
 
 async function publishSatoshiPackage() {
-  const signer = await createFundedHouseAccount();
-
+  const { signer, address, privKey } = await createFundedHouseAccount();
   const publishTxn = await signer.publish({
     compiledModules: bytecode,
     gasBudget: 10000,
@@ -55,11 +48,12 @@ async function publishSatoshiPackage() {
 
     const packageAddress = newObjects?.publish?.packageId;
     console.log("packageAddress", packageAddress);
+    return { packageAddress, houseAddress: address, housePrivateKey: privKey };
   } else {
     console.log("failed to publish package");
   }
 }
 
-
+publishSatoshiPackage();
 
 module.exports.publishSatoshiPackage = publishSatoshiPackage;
