931d3f1a1fc84dbc2def5b5947939dbb46fa4d1c
Stefanos Pleros <stefanos@mystenlabs.com>
2022-12-05 13:35:57 +0200

    Parameterized read and write functions. Now writing both the ui and api envs and filling any missing values with the default

--- scripts/dev/setEnv.js
+++ scripts/dev/setEnv.js
@@ -4,7 +4,22 @@ const { Ed25519PublicKey } = require("@mysten/sui.js");
 const fs = require("fs");
 const { deploy } = require("./deploy_contract");
 
-const defaultEnvValues = {
+const envAPIScheme = {
+  PORT: "",
+  PRIVATE_KEY: "",
+  BANKER_ADDRESS: "",
+  PACKAGE_ADDRESS: "",
+  environment: "",
+  trustedOriginsDev: "",
+  trustedOriginsProd: "",
+};
+
+const envUIScheme = {
+  NEXT_PUBLIC_PACKAGE: "",
+  NEXT_PUBLIC_API_BASE_URL: "",
+};
+
+const defaultAPIEnvValues = {
   PORT: 8080,
   PRIVATE_KEY: "",
   BANKER_ADDRESS: "",
@@ -14,39 +29,43 @@ const defaultEnvValues = {
   trustedOriginsProd: '["https://satoshidomain.sui:3000"]',
 };
 
-function getEnvJson() {
-  const envJson = {
-    PORT: "",
-    PRIVATE_KEY: "",
-    BANKER_ADDRESS: "",
-    PACKAGE_ADDRESS: "",
-    environment: "",
-    trustedOriginsDev: "",
-    trustedOriginsProd: "",
-  };
-  if (!fs.existsSync("./../api/.env")) {
-    execSync("touch ./../api/.env");
+const defaultUIEnvValues = {
+  NEXT_PUBLIC_PACKAGE: "",
+  NEXT_PUBLIC_API_BASE_URL: "http://localhost:8080",
+};
+
+function getEnvJson(folderName = "api") {
+  const envJson = folderName === "api" ? envAPIScheme : envUIScheme;
+  const defaultJson =
+    folderName === "api" ? defaultAPIEnvValues : defaultUIEnvValues;
+
+  if (!fs.existsSync(`./../${folderName}/.env`)) {
+    execSync(`touch ./../${folderName}/.env`);
     writeEnv(envJson);
   }
-  const data = fs.readFileSync("./../api/.env", { encoding: "utf-8" });
+  const data = fs.readFileSync(`./../${folderName}/.env`, {
+    encoding: "utf-8",
+  });
   const lines = data.split("\n");
   for (let line of lines) {
-    if (line.indexOf("=") >= 0) {
+    if (line.indexOf("=") > 0) {
       const [key, value] = line.split("=");
-      envJson[key] = value || defaultEnvValues[key];
+      envJson[key] = value || defaultJson[key];
     }
   }
   return envJson;
 }
 
-function writeEnv(envJson) {
+function writeEnv(envJson, folderName = "api") {
+  const defautValues =
+    folderName === "api" ? defaultAPIEnvValues : defaultUIEnvValues;
   let env = "";
   for (let [key, value] of Object.entries(envJson)) {
     if (Array.isArray(value)) env += `${key}=[${value}]\n`;
-    else env += `${key}=${value}\n`;
+    else env += `${key}=${value || defautValues[key]}\n`;
   }
 
-  fs.writeFileSync("./../api/.env", env);
+  fs.writeFileSync(`./../${folderName}/.env`, env);
 }
 
 function main() {
@@ -70,15 +89,22 @@ function main() {
   const pubKeyClass = new Ed25519PublicKey(pubKeyArr);
   const pubkey = `0x${pubKeyClass.toSuiAddress()}`;
 
-  const envJson = getEnvJson();
-  envJson.BANKER_ADDRESS = pubkey;
-  envJson.PRIVATE_KEY = privKeyArr;
-  writeEnv(envJson);
-  if (envJson.PACKAGE_ADDRESS == null || envJson.PACKAGE_ADDRESS === "") {
+  const envAPIJson = getEnvJson("api");
+  const envUIJson = getEnvJson("ui");
+  envAPIJson.BANKER_ADDRESS = pubkey;
+  envAPIJson.PRIVATE_KEY = privKeyArr;
+  // write initial api and ui env (if it doesn't exist) because it is needed for the deployment
+  writeEnv(envAPIJson, "api");
+  envUIJson.NEXT_PUBLIC_PACKAGE = envAPIJson.PACKAGE_ADDRESS;
+  writeEnv(envUIJson, "ui");
+  if (envAPIJson.PACKAGE_ADDRESS == null || envAPIJson.PACKAGE_ADDRESS === "") {
     const newPackageAddress = deploy();
-    envJson.PACKAGE_ADDRESS = newPackageAddress;
+    envAPIJson.PACKAGE_ADDRESS = newPackageAddress;
+    envUIJson.NEXT_PUBLIC_PACKAGE = newPackageAddress;
+    // if we have new contract deployment update the package address on the api and on ui
+    writeEnv(envAPIJson, "api");
+    writeEnv(envUIJson, "ui");
   }
-  writeEnv(envJson);
 }
 
 main();
