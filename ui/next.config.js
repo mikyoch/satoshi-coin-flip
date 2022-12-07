be274ca72979204d3547cd3d47958d960edd8f59
alex <alex@mystenlabs.com>
2022-12-07 19:11:19 +0200

    Fix next.config.js a bit

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,15 +2,15 @@ const withReactSvg = require('next-react-svg')
 const path = require('path')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
 
 module.exports = withReactSvg({
   include: path.resolve(__dirname, 'public/svg'),
   webpack(config, options) {
     return config
   }
-})
+});
+module.exports.reactStrictMode = true;
+// module.exports.env = {
+//   PACKAGE_ADDRESS: "0xa0ec2d97c19aa4c71c51ff4588b22bb1b216b01a",
+//   API_URL: "http://satoshi-flip-api.satoshi-flip.svc.cluster.local:8080"
+// }
