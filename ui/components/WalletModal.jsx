3f9749fe89094eceda5437ceb23de3b26ba6335b
Alina-chan <alinabalaur@hotmail.com>
2022-12-12 16:57:22 +0200

    Update notices messages

--- ui/components/WalletModal.jsx
+++ ui/components/WalletModal.jsx
@@ -130,7 +130,7 @@ const WalletModal = () => {
       </div>
       {open ? (
         <>
-          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-sui-ocean-dark bg-opacity-50">
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-sui-ocean-dark bg-opacity-50 selection:bg-sui-sky/40 selection:text-white/90">
             <div className="relative w-auto my-4 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-sui-ocean outline-none focus:outline-none">
                 <div className="flex items-start justify-end p-2.5">
@@ -210,9 +210,7 @@ const WalletModal = () => {
                                 </svg>
                               </span>
                               <div className="flex flex-col px-3">
-                                <span>
-                                  Oops! We can't find an available wallet.
-                                </span>
+                                <span>We can't find an available wallet.</span>
                                 <span>Install a wallet to continue.</span>
                                 <div className="flex -ml-2 pt-6">
                                   <div className="border border-sui-sky/50 rounded-full px-3 py-1">
